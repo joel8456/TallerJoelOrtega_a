2c7b284435ae45d319b32f365851d3c48a01f72e
joel8456 <135990010+joel8456@users.noreply.github.com>
2023-06-15 09:44:09 -0500

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,7 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from algebra import distancia
+print(sumar(10, 15))
+print(restar(20, 16))
+print(multiplicar(2, 4))
+print(dividir(10, 6))
+print(distancia(1, 2, 3, 4))
